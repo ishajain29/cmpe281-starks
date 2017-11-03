54a2ea5a931f04f71dd675e352210dcf8b93b53f
Anuj Chaudhari <chaudharianuj93@gmail.com>
2017-11-02 17:48:59 -0700

    remove products from db when removed from ui

--- web-app/js/shoppingCart.js
+++ web-app/js/shoppingCart.js
@@ -8,3 +8,21 @@ function scCartSelectionChanged(e){
     var event = new Event('productsloaded');
     document.dispatchEvent(event);
 }
+
+
+function scSendRequest(strType, strUrl, productData, callback){
+    $.ajax({
+            type: strType,
+            url: strUrl, 
+            data: productData,
+            error: function(xhr, status, error) {
+                console.log("Error While Adding Product to cart ", xhr.responseText);
+                callback(true);
+             },
+            success: function(result) {
+                console.log("Response Arrived !!!!!!!!!", result);
+                callback(true);
+            },
+            dataType: "json"
+        });
+}
