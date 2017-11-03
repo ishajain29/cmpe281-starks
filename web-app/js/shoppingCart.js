1ac607e5d9542dfd12a5dcd07102138ab4da9665
Anuj Chaudhari <chaudharianuj93@gmail.com>
2017-11-02 17:16:47 -0700

    use product id of product catalogue to add items in cart instead of manual fix product id

--- web-app/js/shoppingCart.js
+++ web-app/js/shoppingCart.js
@@ -3,19 +3,6 @@ var scTempUserId = "anuj";
 
 var scSelectedCartIndex = 0;
 
-function scGetUserShoppingCart(){
-    $.ajax({
-        url: scShoppingCartServerURL + '/user/anuj',
-        error: function(xhr, status, error) {
-           console.log(xhr.responseText);
-        },
-        success: function(data) {
-
-        },
-        dataType: "json"
-     });
-}
-
 function scCartSelectionChanged(e){
     scSelectedCartIndex =  parseInt(e.target.id);
     var event = new Event('productsloaded');
