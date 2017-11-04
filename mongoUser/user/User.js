f2cb7eb00aa1eacb533ad3e93ee7d6487f9a4f0b
ishajain29 <isha.jain29@gmail.com>
2017-11-03 22:14:01 -0700

    Revert "added hashing to the password"
    This reverts commit 00028de779013c0491b9c265c23bd4c85df71588.

--- mongoUser/user/User.js
+++ mongoUser/user/User.js
@@ -1,37 +1,9 @@
 var mongoose = require('mongoose');  
-var bcrypt = require('bcrypt'),
-	SALT_WORK_FACTOR = 10;
-
 var UserSchema = new mongoose.Schema({  
   firstname: String,
   lastname: String,
   email: String,
   password: String
 });
-
-UserSchema.pre('save', function (next) {
-	var user = this;
-
-	if(!user.isModified('password')) return next();
-
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-		if (err) return next(err);
-
-		bcrypt.hash(user.password, salt, function (err, hash) {
-			if(err) return next(err);
-
-			user.password = hash;
-			next();
-		});
-	});
-});
-
-UserSchema.methods.comparePassword = function (candidatePassword, callback) {
-	bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-		if(err) return callback(err);
-		callback(undefined, isMatch);
-	});
-};
-
 mongoose.model('User', UserSchema);
 module.exports = mongoose.model('User');
