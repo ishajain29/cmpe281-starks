3332a1165be2e432a079adc809c98843200d1884
Ritika Chadha <ritikachadha05@gmail.com>
2017-11-13 16:16:52 -0800

    Update app.js

--- activity-logs/activity-logs/app.js
+++ activity-logs/activity-logs/app.js
@@ -15,7 +15,11 @@ createError = require('http-errors');
 
 
 var app = express();
-
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
